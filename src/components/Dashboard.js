5bdadefc000a09e2c1bbaedd2683514d1d1d7394
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-03-24 21:29:06 -0700

    amore dependencies. components for Home and Dashboard works with routes

---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div>
+        <h1>Dashboard1</h1>
+        <h1>Dashboard2</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
