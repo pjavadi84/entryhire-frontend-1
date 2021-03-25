28ff522f22ebbe1e6fb5c65c3605c77c96e858a2
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-03-25 01:00:34 -0700

    importing Grid for the form

--- src/components/registration/auth/Registration.js
+++ src/components/registration/auth/Registration.js
@@ -1,4 +1,19 @@
 import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+
+const styles = (theme) => ({
+  root: {
+    margin: theme.spacing.unit * 8,
+    padding: theme.spacing.unit * 3,
+  },
+  item: {
+    padding: theme.spacing.unit * 2,
+  },
+});
 
 class Registration extends Component {
   constructor(props) {
@@ -12,9 +27,62 @@ class Registration extends Component {
     };
   }
 
+  handleChange = (name) => (event) => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
   render() {
-    return <div>Registration goes here</div>;
+    const { classes } = this.props;
+    const { email, password } = this.state;
+
+    return (
+      <div>
+        <div className={classes.root}>
+          <Grid container direction="column" alignItems="center">
+            <Grid item xs={12}>
+              <form>
+                <Grid item xs={12}>
+                  <Typography
+                    className={classes.item}
+                    gutterBottom
+                    variant="title"
+                  >
+                    Add New User
+                  </Typography>
+                </Grid>
+                <Grid item className={classes.item}>
+                  <TextField
+                    label="email"
+                    handleChange={this.handleChange("email")}
+                    value={email}
+                  />
+                </Grid>
+                <Grid item className={classes.item}>
+                  <TextField
+                    label="password"
+                    handleChange={this.handleChange("password")}
+                    value={password}
+                  />
+                </Grid>
+                <Grid item className={classes.item}>
+                  <TextField
+                    label="password_confirmation"
+                    handleChange={this.handleChange("password_confirmation")}
+                  />
+                </Grid>
+              </form>
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Registration;
+Registration.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Registration);
