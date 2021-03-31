68e319b947b3469c96e624fd1e09b7fc75e17224
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-03-30 21:47:18 -0700

    built the user login and setting up cookies to track the partner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import axios from "axios";
 import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
 
@@ -17,9 +18,38 @@ export default class App extends Component {
 
   handleLogin(data) {
     this.setState({
-      loggedInStatus: "Logged in!",
+      loggedInStatus: "successfully Logged in!",
       partner: data.partner,
     });
+  }
+
+  checkLoginStatus() {
+    axios
+      .get("http://localhost:3001/v1/logged_in", { withCredentials: true })
+      .then((response) => {
+        if (
+          response.data.logged_in &&
+          this.state.loggedInStatus === "NOT_LOGGED_IN"
+        ) {
+          this.setState({
+            loggedInStatus: "Logged In!!!",
+            partner: response.data.partner,
+          });
+        } else if (
+          !response.data.logged_in &
+          (this.state.loggedInStatus === "LOGGED_IN")
+        ) {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN",
+            partner: {},
+          });
+        }
+      })
+      .catch((error) => console.log("login error", error));
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
   }
 
   render() {
